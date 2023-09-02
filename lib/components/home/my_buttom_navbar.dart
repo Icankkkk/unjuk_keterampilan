312409cc0537700e776221fe6f7a542867597104
afrizal prayogi <61342260+Icankkkk@users.noreply.github.com>
2023-09-03 05:55:32 +0700

    create navbar compoment

---
+++ lib/components/home/my_buttom_navbar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyButtomNavbar extends StatelessWidget {
+  const MyButtomNavbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:unjuk_keterampilan/components/home/header.dart';
 import 'package:unjuk_keterampilan/components/home/today_card.dart';
+import 'package:unjuk_keterampilan/config/app_color.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -8,6 +9,25 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Favorite',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.money),
+            label: 'Money',
+          ),
+        ],
+        currentIndex: 0,
+        selectedItemColor: AppColor.lev1,
+        onTap: (value) {},
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(24, 10, 24, 10),
@@ -21,10 +41,9 @@ class HomePage extends StatelessWidget {
                   children: const [
                     // Pengeluaran hari ini
                     Today(),
-                    // Pengeluaran hari ini
-                    Today(),
-                    // Pengeluaran hari ini
-                    Today(),
+                    // Pengeluaran minggu ini
+
+                    // Perbandingan bulan ini
                   ],
                 ),
               ),
